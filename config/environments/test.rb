9237d2c2eb8f528ae4638facb790c2d17259fd82
evsasse <evsasse@gmail.com>
2019-10-27 13:13:27 -0300

    prettier for js and rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -20,7 +20,7 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
